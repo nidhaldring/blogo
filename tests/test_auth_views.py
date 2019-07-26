9e8f76f6fc64650ecca44c39fd7720b227881bcd
nidhaldring <dringthedreamer@gmail.com>
2019-07-26 20:09:38 +0100

    refactored old tests + added tests for login view

--- tests/test_auth_views.py
+++ tests/test_auth_views.py
@@ -9,39 +9,38 @@ from config import TestingConfig
 
 index_ = "auth.index"
 
-class TestAuthViews(unittest.TestCase):
+class TestAuthViews(unittest.TestCase):	
 
-	def test_register_view(self):
+	def __init__(self,*args,**kargs):
 
-		app = createApp(TestingConfig)
-		with app.test_client() as client:
+		super().__init__(*args,**kargs)
+
+		self.app = createApp(TestingConfig)
+		self.userData = dict(username="testu",password="testu",email="testu@test.com")
+
+	def test_a_register_view(self):
+
+		with self.app.test_client() as client:
 			
 			resp = client.get("/auth/register",follow_redirects=True)
 			self.assertEqual(200,resp.status_code)
 
-			resp = client.post("/auth/register",data=dict(
-					username="testu",
-					password="testu",
-					email="testu@test.com"
-				),
-				follow_redirects=True
-			)
+			resp = client.post("/auth/register",data=self.userData,follow_redirects=True)
 			self.assertEqual(200,resp.status_code)
 			self.assertEqual(index_,request.endpoint)
 			self.assertTrue("_user_id" in session)
 
-	'''
-	def test_login_lougout_views(self):
 
-		app = createApp(TestingConfig)
-		with app.test_client() as client:
+	def test_b_login_view(self):
+
+		with self.app.test_client() as client:
 
 			resp = client.get("/auth/login")
 			self.assertEqual(resp.status_code,200)
 
 			resp = client.post("/auth/login",data=dict(
-					username="nidhal",
-					password="123"
+					username=self.userData["username"],
+					password=self.userData["password"]
 				),
 				follow_redirects=True
 			)
@@ -49,8 +48,13 @@ class TestAuthViews(unittest.TestCase):
 			self.assertEqual(request.endpoint,index_)
 			self.assertTrue("_user_id" in session)
 
-			resp = client.get("auth/logout",follow_redirects=True)
-			self.assertEqual(resp.status_code,200)
-			self.assertEqual(request.endpoint,index_)
-			self.assertFalse("_user_id" in session)
-	'''
+	def test_c_logout_view(self):
+
+		with self.app.test_client() as client:
+
+			resp = client.get("/auth/logout",follow_redirects=True)
+			self.assertEqual(index_,request.endpoint)
+			self.assertEqual(200,resp.status_code)
+			self.assertTrue("_user_id" not in session)
+
+
