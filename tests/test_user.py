bd68638ed1e2f21dce2a79c5d6591abc008515a3
nidhaldring <dringthedreamer@gmail.com>
2019-07-28 15:33:12 +0100

    added more tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,6 +3,7 @@
 import unittest
 
 import pymysql
+from werkzeug.security import check_password_hash
 
 from models.user import *
 
@@ -26,19 +27,30 @@ class TestUser(unittest.TestCase):
 	def test_register_delete_methods(self):
 
 		u = User("n","n","g@g.com")
-
 		self.assertRaises(UserNotRegistredException,u.delete)
 
 		u.register()
-		u = User.query({"email":"g@g.com"})[0]
+		u = User.query({"email":"g@g.com"})
 
+		# assert registration
 		self.assertNotEqual(u,[])
+
+		# assert a unique email per user
+		self.assertEqual(len(u),1) 
+
+		# assert data is retreived correctly
+		u = u[0]
 		self.assertEqual(u.username,"n")
-		self.assertEqual(u.password,"n")
+		self.assertTrue(check_password_hash(u.password,"n"))		
 		self.assertEqual(u.email,"g@g.com")
 
+		# assert register method works only one time
+		self.assertRaises(UserAlreadyRegistredException,u.register)
+
+		self.assertRaises(EmailAlreadyExistsException,User("n","n","g@g.com").register)
+
 		u.delete()
-		self.assertEqual(User.query(dict(email="x")),[])
+		self.assertEqual(User.query({"email":"g@g.com"}),[])
 
 
 
