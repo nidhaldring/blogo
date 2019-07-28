a3839940373409c4eb21bd3ccf650e7992ed5a19
nidhaldring <dringthedreamer@gmail.com>
2019-07-28 15:32:46 +0100

    fixed delete & added id as a data property

--- models/user.py
+++ models/user.py
@@ -1,14 +1,25 @@
 
 import pymysql
+from werkzeug.security import generate_password_hash
+
 from config import Config 
 
 
-class UserNotRegistredException(Exception):
+# exceptions
 
+class UserAlreadyRegistredException(Exception):
 	def __init__(self):
+		self.message = "The user is already registred !"
 
+class UserNotRegistredException(Exception):
+	def __init__(self):
 		self.message = "The user is not yet registered on the db"
 
+class EmailAlreadyExistsException(Exception):
+	def __init__(self):
+		self.manage = "This email is already in use !"
+
+#
 
 
 class User:
@@ -22,19 +33,33 @@ class User:
 
 	def __init__(self,username,password,email,id_=None):
 
-		self.id = id_
+		self._id = id_
 		self.username = username
 		self.password = password
 		self.email = email 
 
 
+	@property
+	def id(self):
+		return self._id
+	
+
 	def register(self):
+
+		if self._id is not None:
+			raise UserAlreadyRegistredException()
 
 		conn = pymysql.connect(**self.DB)
 		sql = f"insert into {self.TABLE}(username,password,email) values(%s,%s,%s)"
+		args = (self.username,generate_password_hash(self.password),self.email)
 		cursor = conn.cursor()
 
-		cursor.execute(sql,(self.username,self.password,self.email))
+		try:
+			cursor.execute(sql,args)
+		except pymysql.err.IntegrityError as e:
+			if e.args[0] == 1062:
+				raise EmailAlreadyExistsException()
+			raise e
 
 		conn.commit()
 		conn.close()
@@ -43,23 +68,27 @@ class User:
 		# and set the id  
 		# so delete op can be possible from now on
 		currentUser = self.query(dict(email=self.email))[0]
-		self.id = int(currentUser.id)
+		self._id = int(currentUser.id)
+
+		return self
 
 	def delete(self):
 
-		if self.id is None:
-			
+		if self._id is None:	
 			raise UserNotRegistredException() 
 
 		conn = pymysql.connect(**self.DB)
 		sql = f"delete from {self.TABLE} where id=%s"
 		cursor = conn.cursor()
 
-		cursor.execute(sql,(self.id,))
+		cursor.execute(sql,(self._id,))
 
 		conn.commit()
 		conn.close()
 
+		self._id = None
+
+		return self
 
 	@classmethod
 	def query(cls,cond:dict) -> list:
@@ -79,4 +108,4 @@ class User:
 
 	# for debugging purposes
 	def __str__(self):
-		return f"{self.username} [{self.id}]"
+		return f"{self.username} [{self._id}]"
