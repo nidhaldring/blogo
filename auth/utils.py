7c4c8951c971214654d850a7719463a3ef7392ee
nidhaldring <dringthedreamer@gmail.com>
2019-07-29 12:11:46 +0100

    refactoring

---
+++ auth/utils.py
@@ -0,0 +1,48 @@
+
+from functools import wraps
+
+from flask import session,current_app,redirect,url_for,abort
+
+from models.user import User,UserNotRegistredException
+
+
+currentUser = None
+
+def getCurrentUser():
+	return currentUser
+
+
+# add support for remember me feature later
+def loginUser(user):
+
+	if not user.id:
+		raise UserNotRegistredException()
+
+	session["_user_id"] = user.id
+
+	global currentUser
+	currentUser = user
+
+
+def logoutUser():
+
+	session.clear()
+
+	global currentUser
+	currentUser = None
+
+
+
+def loginRequired(view):
+	
+	@wraps(view)
+	def decorator(*args,**kargs):
+		if getCurrentUser() is None:
+			redirectUrl = current_app.config.get("REDIRECT_URL",None)
+			if redirectUrl:
+				return redirect(url_for(redirectUrl))
+
+			abort(401) # unathorized
+
+		return view(*args,**kargs)
+	return decorator
