8c5353b7b07c517c3351dc9808db73fd4c32b545
nidhaldring <dringthedreamer@gmail.com>
2019-07-30 18:02:27 +0100

    added generic update & insert & delete

--- models/utils.py
+++ models/utils.py
@@ -2,7 +2,7 @@
 import pymysql
 
 
-def executeSQL(sql,dbCon:dict):
+def executeSQL(dbCon:dict,sql):
 
 	'''
 	executes the given sql
@@ -20,11 +20,45 @@ def executeSQL(sql,dbCon:dict):
 
 	return res
 
+def createCond(cond:dict):
+	
+	'''
+	returns the cond part of the where
+	'''
+
+	return " and ".join(["{} = '{}' ".format(i,j) for i,j in cond.items()])
+
+
+
+def insert(dbCon,table,data:dict):
+
+	sql = f"insert into {table}"
+	sql += "(" + " , ".join(data.keys()) +")"
+	sql += " values(" + " , ".join([f"'{i}'" for i in data.values()]) + ")"
+
+	executeSQL(dbCon,sql)
+
+def delete(dbCon,table,cond:dict):
+
+	cond = createCond(cond)
+	sql = f"delete from {table} where " + cond
+
+	executeSQL(dbCon,sql)
+
+
+def update(dbCon,table,id_,newData:dict):
+
+	sql = f"update {table} set "
+	sql +=  " , ".join(["{} = '{}' ".format(i,j) for i,j in newData.items()])
+	sql += f"where id = {id_}"
+
+	executeSQL(dbCon,sql)
+
+
 
 def query(dbCon,table,cond:dict) -> list:
 
-	cond = " and ".join(["{} = '{}' ".format(i,j) for i,j in cond.items()])
+	cond = createCond(cond)
 	sql = f"select * from {table} where " + cond
-	res = executeSQL(sql,dbCon)
 
-	return res
+	return  executeSQL(dbCon,sql)
