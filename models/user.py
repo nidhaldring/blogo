402f2d5314ad0d799a6e53d3bd64420c3231237c
nidhaldring <dringthedreamer@gmail.com>
2019-07-30 18:01:00 +0100

    deleted password property & fixed update

--- models/user.py
+++ models/user.py
@@ -3,17 +3,15 @@ from werkzeug.security import generate_password_hash
 import pymysql
 
 from config import Config 
-from models.utils import executeSQL,query
+from models.utils import query,insert,delete,update
 from models.exceptions import UserAlreadyRegistredException,UserNotRegistredException,EmailAlreadyExistsException
 					
 
 class User:
 
-	'''
-	manages a user in db
-	'''
+	'''manages a user in db'''
 
-	DB = dict(db=Config.DB_NAME,user="root",password="root",host="localhost")
+	DB_CON = dict(db=Config.DB_NAME,user="root",password="root",host="localhost")
 	TABLE = "users"
 
 	# id_ should only be set internally
@@ -31,34 +29,30 @@ class User:
 	
 	@id.setter
 	def id(self,v):
-		raise AttributeError("can't set id !")
+		raise AttributeError("Can't set id !")
 	
-
 	@classmethod
 	def query(cls,cond:dict) -> list:
 
-		res = query(cls.DB,cls.TABLE,cond)
+		res = query(cls.DB_CON,cls.TABLE,cond)
 		return [cls(row[1],row[2],row[3],row[0]) for row in res] 
+
 
 	def register(self):
 
 		if self._id is not None:
 			raise UserAlreadyRegistredException()
 
-		sql = (f"insert into {self.TABLE}(username,password,email)" 
-			+ f" values('{self.username}'," 
-			+ f"'{generate_password_hash(self.password)}','{self.email}');")
-	
 		try:
-			executeSQL(sql,self.DB)
+			data = dict(username=self.username,password=generate_password_hash(self.password),email=self.email)
+			insert(self.DB_CON,self.TABLE,data)
 		except pymysql.err.IntegrityError as e:
 			if e.args[0] == 1062:
 				raise EmailAlreadyExistsException()
 			raise e
 
-		# fetch the current user from db 
-		# and set the id  
-		# so delete op can be possible from now on
+		# set the id  
+		# so delete/update op can be possible from now on
 		currentUser = self.query(dict(email=self.email))[0]
 		self._id = int(currentUser.id)
 
@@ -69,8 +63,7 @@ class User:
 		if self._id is None:	
 			raise UserNotRegistredException() 
 
-		sql = f"delete from {self.TABLE} where id='{self._id}'"
-		executeSQL(sql,self.DB)
+		delete(self.DB_CON,self.TABLE,{"id":self._id})
 
 		self._id = None
 
@@ -78,24 +71,21 @@ class User:
 
 	def update(self,newData):
 
-		'''
-		returns the new updated user
-		dosn't change the user in place !
-		'''
+		'''returns the new updated user'''
 
 		if self.id is None:
 			raise UserNotRegistredException()
+
+		# hash the password
+		if "password" in newData:
+			newData.update({"password":generate_password_hash(newData["password"])})
 
 		# update the current object
 		self.__dict__.update(newData)
 
 		#update db
-		sql = f"update {self.TABLE} set "
-		sql +=  " , ".join(["{} = '{}' ".format(i,j) for i,j in newData.items()])
-		sql += f"where id = {self.id}"
-
 		try:
-			executeSQL(sql,self.DB)
+			update(self.DB_CON,self.TABLE,self._id,newData)
 		except pymysql.err.IntegrityError as e:
 			if e.args[0] == 1062:
 				raise EmailAlreadyExistsException()
@@ -106,5 +96,5 @@ class User:
 
 
 	# for debugging purposes
-	def __str__(self):
+	def __repr__(self):
 		return f"{self.username} [{self._id}]"
