be29d90705f3959211f0931e298acd168775bd77
nidhaldring <dringthedreamer@gmail.com>
2019-07-31 12:56:05 +0100

    moved update & insert & delete to model class

--- models/utils.py
+++ models/utils.py
@@ -29,33 +29,6 @@ def createCond(cond:dict):
 	return " and ".join(["{} = '{}' ".format(i,j) for i,j in cond.items()])
 
 
-
-def insert(dbCon,table,data:dict):
-
-	sql = f"insert into {table}"
-	sql += "(" + " , ".join(data.keys()) +")"
-	sql += " values(" + " , ".join([f"'{i}'" for i in data.values()]) + ")"
-
-	executeSQL(dbCon,sql)
-
-def delete(dbCon,table,cond:dict):
-
-	cond = createCond(cond)
-	sql = f"delete from {table} where " + cond
-
-	executeSQL(dbCon,sql)
-
-
-def update(dbCon,table,id_,newData:dict):
-
-	sql = f"update {table} set "
-	sql +=  " , ".join(["{} = '{}' ".format(i,j) for i,j in newData.items()])
-	sql += f"where id = {id_}"
-
-	executeSQL(dbCon,sql)
-
-
-
 def query(dbCon,table,cond:dict) -> list:
 
 	cond = createCond(cond)
