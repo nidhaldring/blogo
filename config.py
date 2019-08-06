b4251211e5231ab11e18f2ff9f3d7bea0ef2dbe7
nidhaldring <dringthedreamer@gmail.com>
2019-08-07 00:13:35 +0100

    added more configs for db

--- config.py
+++ config.py
@@ -7,10 +7,11 @@ class Config:
 	TESTING = False
 	SECRET_KEY = os.urandom(10)
 	DB_NAME = "blog"
+	USERS_TABLE = "root"
+	DB_PASSWORD = "root"
+	DB_HOST = "localhost"
 
 
-class TestingConfig:
+class TestingConfig(Config):
 
 	TESTING = True
-	SECRET_KEY = os.urandom(10)
-	DB_NAME ="blog"
