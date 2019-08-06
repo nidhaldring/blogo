c5ea54b28bd4c6b55c597c8d3c099d880df89a69
nidhaldring <dringthedreamer@gmail.com>
2019-08-07 00:14:17 +0100

    added sqlQueryMaker tests to be run & disabled views tests

--- runTests.py
+++ runTests.py
@@ -1,9 +1,10 @@
 import unittest
 
 from tests.test_user import *
-from tests.test_auth_views import *
-from tests.test_post import *
-from tests.test_posts_views import *
+from tests.test_sqlQueryMaker import *
+#from tests.test_auth_views import *
+#from tests.test_post import *
+#from tests.test_posts_views import *
 
 
 unittest.main()
