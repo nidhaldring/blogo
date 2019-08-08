9d19b8b90e8e0ec5c3d8b544c7e93f5bbca78642
nidhaldring <dringthedreamer@gmail.com>
2019-08-08 21:20:24 +0100

    created tests for query

--- tests/test_post.py
+++ tests/test_post.py
@@ -2,43 +2,32 @@
 
 import unittest
 
-import pymysql
-
 from models.post import Post
-from models.user import User
 from models.exceptions import *
+from tests.mockObjects import *
 
 
-Post.DB_CON = dict(host="localhost",db="test",user="root",password="root")
+# mock objects
+class Author:
+	pass
+
+class MockDbManager:
+	def execute(self,sql):
+		return [("1","1","1","1")]
+
+#
 
 class TestPost(unittest.TestCase):
 
+	def __init__(self,*args,**kargs):
+		super().__init__(*args,**kargs)
+		Post.dbManager = MockDbManager()
+		Post.queryMaker = MockQueryMaker()
 
 	def setUp(self):
 
-		self.author = User("f","f","f@f.com").register()
-		self.post = Post("title","body",self.author.id)
-
-	def tearDown(self):
-
-		conn = pymysql.connect(**Post.DB_CON)
-		cursor = conn.cursor()
-		cursor.execute("delete from posts;")
-		cursor.execute("delete from users;")
-		conn.commit()
-		conn.close()
-
-
-	def test_insert_update_db(self):
-		
-		self.post.insert()
-		posts = Post.query({"id":self.post.id})
-		self.assertTrue(len(posts) == 1)
-
-		post = posts[0]
-		self.assertEqual(post.body,self.post.body)
-		self.assertEqual(post.title,self.post.title)
-		self.assertEqual(post.id,self.post.id)
+		self.author = Author()
+		self.post = Post("title","body",self.author)
 
 	def test_insert_raise_when_reinserted(self):
 
@@ -49,12 +38,6 @@ class TestPost(unittest.TestCase):
 
 		self.post.insert()
 		self.assertFalse(self.post.id is None)
-
-	def test_delete_update_db(self):
-
-		self.post.insert()
-		self.post.delete()
-		self.assertEqual(Post.query({"id":self.post.id}),[])
 
 	def test_delete_raises_when_deleting_non_inserted_posts(self):
 
@@ -67,22 +50,20 @@ class TestPost(unittest.TestCase):
 		self.assertEqual(self.post.id,None)
 
 
-	def test_update_changes_values(self):
+	def test_update_changes_object_values_inplace(self):
 
 		self.post.insert()
 		self.post.update({"body":"ttt"})
 		self.assertEqual(self.post.body,"ttt")
 
-	def test_update_update_db(self):
-
-		self.post.insert()
-		self.post.update({"body":"ttt"})
-		post = Post.query({"id":self.post.id})[0]
-
-		self.assertEqual(post.body,"ttt")
-
 	def test_update_raises_when_updating_uninserted_posts(self):
 
 		self.assertRaises(PostNotInsertedException,self.post.update,{"body":"5"})
+
+	def test_query_returns_list_of_post_objects_with_correct_query_condition(self):
+
+		posts = Post.query({"title":"1"})
+		self.assertNotEqual(posts,[])
+		self.assertEqual(posts[0].title,"1")
 
 
