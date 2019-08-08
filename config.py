c215345a2042b4a4d7e06dcadddd4d10d1bdfa22
nidhaldring <dringthedreamer@gmail.com>
2019-08-08 21:19:11 +0100

    added table settings for post table

--- config.py
+++ config.py
@@ -7,9 +7,10 @@ class Config:
 	TESTING = False
 	SECRET_KEY = os.urandom(10)
 	DB_NAME = "blog"
-	USERS_TABLE = "root"
 	DB_PASSWORD = "root"
 	DB_HOST = "localhost"
+	USERS_TABLE = "users"
+	POSTS_TABLE = "posts"
 
 
 class TestingConfig(Config):
