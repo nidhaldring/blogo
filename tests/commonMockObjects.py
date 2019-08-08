de8a289f54d7b27d457a08a793452c600cbb2390
nidhaldring <dringthedreamer@gmail.com>
2019-08-08 23:37:09 +0100

    fixed an error on MockQueryMaker

--- tests/commonMockObjects.py
+++ tests/commonMockObjects.py
@@ -6,7 +6,7 @@
 class MockQueryMaker:
 	def __init__(self):
 		self.table = ""
-	def makeInsertQuery(self):pass 
+	def makeInsertQuery(self,data):pass 
 	def makeDeleteQuery(self,cond):pass 
 	def makeUpdateQuery(self,data,cond):pass 
 	def makeSearchQuery(self,cond):pass
