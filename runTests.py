1740749d0e2fab80c65349537ec3f3279103fd54
nidhaldring <dringthedreamer@gmail.com>
2019-08-08 21:08:41 +0100

    enabled post tests again

--- runTests.py
+++ runTests.py
@@ -3,7 +3,7 @@ import unittest
 from tests.test_user import *
 from tests.test_sqlQueryMaker import *
 #from tests.test_auth_views import *
-#from tests.test_post import *
+from tests.test_post import *
 #from tests.test_posts_views import *
 
 
