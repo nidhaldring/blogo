aa72015cec5ffc36c9c9984e57c71d98a13379f2
nidhaldring <dringthedreamer@gmail.com>
2019-08-09 22:58:03 +0100

    removed unecessary tests

--- tests/test_posts_views.py
+++ tests/test_posts_views.py
@@ -4,34 +4,21 @@ import unittest
 
 from createApp import createApp
 from config import TestingConfig
-from models.post import Post
-from models.user import User
 
 
 class TestPostsViews(unittest.TestCase):
 
-	def __init__(self,*args,**kargs):
-
-		super().__init__(*args,**kargs)
+	def setUp(self):
 		self.app = createApp(TestingConfig)
 
-
-	def test_posts_view_return_200_when_acessing_posts(self):
-
-		with self.app.test_client() as client:
-
-			author = User("n","f","f@f.com").register()
-			post = Post("ggg","fff",author.id).insert()
-			resp = client.get(f"/posts/{post.id}",follow_redirects=True)
-		
-			post.delete()
-			author.delete()
-
-			self.assertEqual(resp.status_code,200)
-
-
-	def test_posts_view_return_404_when_post_not_exists(self):
+	def test_posts_view_return_404_when_post_does_not_exists(self):
 
 		with self.app.test_client() as client:
-			resp = client.get("/posts/0",follow_redirects=True)
+			resp = client.get("/posts/-1",follow_redirects=True)
 			self.assertEqual(resp.status_code,404)
+
+	def test_create_view_return_401_when_accessed_by_unauthorized_user(self):
+
+		with self.app.test_client() as client:
+			resp = client.get("/posts/create",follow_redirects=True)
+			self.assertEqual(resp.status_code,401) # unauthorized
