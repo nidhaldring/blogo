ae8fe2f16bf14d101992a4931ba3a72d7f5d414e
nidhaldring <dringthedreamer@gmail.com>
2019-08-09 22:58:31 +0100

    enabled post views tests

--- runTests.py
+++ runTests.py
@@ -4,7 +4,7 @@ from tests.test_user import *
 from tests.test_sqlQueryMaker import *
 from tests.test_auth_views import *
 from tests.test_post import *
-#from tests.test_posts_views import *
+from tests.test_posts_views import *
 
 
 unittest.main()
