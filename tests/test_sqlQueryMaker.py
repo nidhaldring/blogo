b082e81b5baa6ed0e414e853b252828a066bdadb
nidhaldring <dringthedreamer@gmail.com>
2019-08-11 19:04:40 +0100

    changed the interface for  MockSQLQueryMaker to support the updated makeSearchQuery

--- tests/test_sqlQueryMaker.py
+++ tests/test_sqlQueryMaker.py
@@ -58,3 +58,26 @@ class TestSQLQueryMaker(unittest.TestCase):
 		expectedQuery = "select * from test where id='5' and v='2';"
 		sql = self.queryMaker.makeSearchQuery({"id":"5","v":"2"})
 		self.assertEqual(expectedQuery,sql)
+
+	def test_makeSearchQuery_with_limit(self):
+		
+		expectedQuery = "select * from test where id='5' limit 5;"
+		sql = self.queryMaker.makeSearchQuery({"id":"5"},limit=5)
+		self.assertEqual(expectedQuery,sql)
+
+	def test_makeSearQuery_raises_when_desc_is_set_to_True_without_giving_orderBy_list(self):
+
+		with self.assertRaises(Exception):
+			self.queryMaker.makeSearchQuery({"id":"5"},desc=True)
+
+	def test_makeSearchQyery_with_orderBy_list(self):
+
+		expectedQuery = "select * from test where id='5' order by s,n;"
+		sql = self.queryMaker.makeSearchQuery({"id":"5"},orderBy=["s","n"])
+		self.assertEqual(expectedQuery,sql)
+
+	def test_makeSearchQuery_with_orderBy_list_and_desc_is_set_to_True(self):
+
+		expectedQuery = "select * from test where id='5' order by s,f desc;"
+		sql = self.queryMaker.makeSearchQuery({"id":"5"},orderBy=["s","f"],desc=True)
+		self.assertEqual(expectedQuery,sql)
