e5f8a906498ce8d0a416f52af9b39593b2ceec97
nidhaldring <dringthedreamer@gmail.com>
2019-10-18 00:36:28 +0100

    refactoring

--- views/home/views.py
+++ views/home/views.py
@@ -1,11 +1,11 @@
 
 
+from flask import render_template
+
 from views.auth.utils import getCurrentUser
 from views.home import bp
-
+from models.post import Post
 
 @bp.route("/")
 def index():
-
-	u = getCurrentUser() or ""
-	return "welcome " + str(u)
+	return "welcome home !"
