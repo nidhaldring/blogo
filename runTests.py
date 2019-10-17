3cffc29a2e8fc3809de2d4daf1c0ec48a7401725
nidhaldring <dringthedreamer@gmail.com>
2019-10-17 23:30:09 +0100

    removed inutil tests

--- runTests.py
+++ runTests.py
@@ -1,10 +1,1 @@
-import unittest
 
-from tests.test_user import *
-from tests.test_sqlQueryMaker import *
-from tests.test_auth_views import *
-from tests.test_post import *
-from tests.test_posts_views import *
-
-
-unittest.main()
