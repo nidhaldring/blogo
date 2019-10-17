d6ea966253a24e448ac93c7d3eac1a119a381a9b
nidhaldring <dringthedreamer@gmail.com>
2019-10-17 16:51:49 +0100

    integraed sqlalchemy & refactored code

--- models/user.py
+++ models/user.py
@@ -1,64 +1,24 @@
 
 from werkzeug.security import generate_password_hash
+from sqlalchemy import *
 
-from config import Config 
-from models.model import Model
-from models.utils.dbManager import DbManager
-from models.utils.sqlQueryMaker import SQLQueryMaker
-from models.exceptions import *
-					
+from config import Config
+from models.model import ModelMixin,Base
 
+class User(Base,ModelMixin):
+	__tablename__ = Config.USERS_TABLE
+	id = Column(Integer,primary_key=True)
+	username = Column(String(64),nullable=False)
+	_password = Column(String(96),nullable=False)
 
-class User(Model):
+	def __init__(self,*,username,password):
+		self.username = username
+		self.password = password
 
-	'''manages a user in db'''
+	@property
+	def password(self):
+		raise AttributeError("can't access password !")
 
-	dbManager = DbManager()
-	queryMaker = SQLQueryMaker(table=Config.USERS_TABLE)
-
-	# id_ should only be set internally
-	def __init__(self,username,password,email,_id=None):
-
-		data = dict(username=username,password=password,email=email)
-		super().__init__(data=data,_id=_id)
-	
-
-	def register(self):
-
-		try:
-			self.data["password"] = generate_password_hash(self.password)
-			return super().insert()
-		except ModelUniqueConstraintException:			
-				raise EmailAlreadyExistsException()
-		except ModelAlreadyInsertedException:
-			raise UserAlreadyRegistredException()
-
-	def delete(self):
-
-		try:
-			return super().delete()
-		except ModelNotInsertedException:
-			raise UserNotRegistredException()
-
-	def update(self,newData):
-
-		# hash the password
-		if "password" in newData:
-			newData.update({"password":generate_password_hash(newData["password"])})
-		
-		try:
-			return super().update(newData)
-		except ModelUniqueConstraintException:
-			raise EmailAlreadyExistsException()
-		except ModelNotInsertedException:
-			raise UserNotRegistredException()
-
-
-	@classmethod
-	def query(cls,cond:dict,limit=None) -> list:
-
-		return [cls(row[1],row[2],row[3],_id=row[0]) for row in cls._search(cond,limit)] 
-
-	# for debugging purposes
-	def __repr__(self):
-		return f"{self.username} [{self._id}]"
+	@password.setter
+	def password(self,p:str):
+		self._password = generate_password_hash(p)
