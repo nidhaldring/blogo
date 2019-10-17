e3cb4144811aff6a52c64bb7a000588ade416f85
nidhaldring <dringthedreamer@gmail.com>
2019-10-17 23:47:05 +0100

    refactoring

--- views/auth/utils.py
+++ views/auth/utils.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from flask import session,abort
 
-from models.user import User,UserNotRegistredException
+from models.user import User
 
 
 currentUser = None
@@ -14,32 +14,23 @@ def getCurrentUser():
 
 # add support for remember me feature later
 def loginUser(user):
-
 	if not user.id:
 		raise UserNotRegistredException()
-
 	session["_user_id"] = user.id
-
 	global currentUser
 	currentUser = user
 
 
 def logoutUser():
-
 	session.clear()
-
 	global currentUser
 	currentUser = None
 
 
-
 def loginRequired(view):
-	
 	@wraps(view)
 	def decorator(*args,**kargs):
 		if getCurrentUser() is None:
 			abort(401) # unathorized
-
 		return view(*args,**kargs)
-
 	return decorator
