41e833dbe1c1b75c117ac38f9babb0a90d0ff121
nidhaldring <dringthedreamer@gmail.com>
2019-10-30 20:59:32 +0100

    added profile prefix

--- createApp.py
+++ createApp.py
@@ -4,6 +4,7 @@ from flask import Flask
 from views.auth import bp as auth_bp
 from views.posts import bp as posts_bp
 from views.home import bp as home_bp
+from views.user import bp as user_bp
 
 
 def createApp(config):
@@ -15,5 +16,6 @@ def createApp(config):
 	app.register_blueprint(auth_bp,url_prefix="/auth")
 	app.register_blueprint(posts_bp,url_prefix="/posts")
 	app.register_blueprint(home_bp,url_prefix="/")
+	app.register_blueprint(user_bp,url_prefix="/profile")
 
 	return app
