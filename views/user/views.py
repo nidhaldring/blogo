9118301ee7bc5100913b094f5a647c44361dddb3
nidhaldring <dringthedreamer@gmail.com>
2019-11-01 21:22:35 +0100

    made profile view handle post method

--- views/user/views.py
+++ views/user/views.py
@@ -1,10 +1,30 @@
+import os
 
-from flask import render_template
+from flask import render_template,request,url_for,redirect
 
 from views.user import bp
 from views.auth.utils import loginRequired,getCurrentUser
+from config import Config
 
-@bp.route("/")
+
+@bp.route("/",methods=["POST","GET"])
 @loginRequired
 def profile():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        profileImg = request.files["profileImg"]
+
+        u = getCurrentUser()
+
+        profileImgPath = os.path.join(Config.UPLOAD_FOLDER,u.email + profileImg.filename)
+        profileImg.save(os.path.join("static",profileImgPath))
+
+        u.email = email
+        u.username = username
+        u.pic = profileImgPath
+        u.insert()
+
+        return redirect(url_for(".profile"))
+
     return render_template("user/profile.html",user=getCurrentUser())
