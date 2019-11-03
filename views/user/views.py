34c5aae980666b926d272506ebd30e83723e7451
nidhaldring <dringthedreamer@gmail.com>
2019-11-03 17:14:50 +0100

    added a check for img extension in profile view

--- views/user/views.py
+++ views/user/views.py
@@ -1,10 +1,10 @@
 import os
 
-from flask import render_template,request,url_for,redirect
+from flask import render_template,request,url_for,redirect,flash
 
 from views.user import bp
 from views.auth.utils import loginRequired,getCurrentUser
-from views.user.utils import updateUser
+from views.user.utils import updateUser,extensionIsAllowed
 from config import Config
 
 
@@ -13,10 +13,15 @@ from config import Config
 def profile():
     if request.method == "POST":
         user = getCurrentUser()
+
         username = request.form["username"]
         email = request.form["email"]
         password = request.form["password"]
         pic = request.files["profileImg"]
+
+        if not extensionIsAllowed(pic):
+            flash(f"the allowed extensions are {' , '.join(Config.ALLOWED_EXTENSIONS)}")
+            return redirect(url_for(".profile"))
 
         updateUser(user,username=username,email=email,password=password,pic=pic)
 
