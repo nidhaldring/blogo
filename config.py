e23475eab7435506436ed2c24fe4882f948fbc0b
nidhaldring <dringthedreamer@gmail.com>
2019-11-03 17:13:43 +0100

    added allowed extensions to config

--- config.py
+++ config.py
@@ -6,8 +6,11 @@ class Config:
 
 	TESTING = False
 	SECRET_KEY = os.urandom(10)
+
 	ENGINE_URI = "mysql+pymysql://localhost/tests"
 	USERS_TABLE = "users"
 	POSTS_TABLE = "posts"
+
 	UPLOAD_FOLDER = "uploads"
 	DEFAULT_USER_PIC_LOCATIONS = "uploads/profile.png"
+	ALLOWED_EXTENSIONS = {"png","jpg","jpeg"}
